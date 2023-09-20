c006d75547c09ce87a753e0d42b73f1ea2654e41
Justin Ireland <justinireland90@yahoo.com>
2023-09-20 10:10:44 -0400

    Fixed bugs, Done with clicking walkers and pets

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -2,11 +2,36 @@ import { getPets } from "./database.js"
 
 const pets = getPets()
 
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+
+        const itemClicked = clickEvent.target
+
+        if (itemClicked.id.startsWith("pet")) {
+            const[,petId] = itemClicked.id.split("--")
+            
+            
+              for (const petObject of pets) {
+
+                if (petObject.id === parseInt(petId))        //petObject.city will be undifined pets have no city
+                    window.alert(`${petObject.name} barks at you`)
+              }  
+
+
+        }
+    }
+    
+)
+
+
+
 export const RegisteredPets = () => {
     let petHTML = "<ul>"
 
     for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
+        // petHTML += `<li>${pet.name}</li>` UPDATED
+        petHTML += `<li id="pet--${pet.id}">${pet.name}</li>` 
     }
 
     petHTML += "</ul>"
