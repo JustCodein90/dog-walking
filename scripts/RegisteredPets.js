12829e418a2c9f80c5729a30ed53435304c5436e
Justin Ireland <justinireland90@yahoo.com>
2023-09-20 14:37:59 -0400

    Added click event to RedisteredPets

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,24 +1,40 @@
 import { getPets } from "./database.js"
+import { getWalkers } from "./database.js"
 
 const pets = getPets()
+const walkers = getWalkers()
 
 document.addEventListener(
     "click",
     (clickEvent) => {
-
+        //HTML click event target
         const itemClicked = clickEvent.target
-
+        
+        // Did the user click on a pet <li>?
         if (itemClicked.id.startsWith("pet")) {
-            const[,petId] = itemClicked.id.split("--")
-            
-            
-              for (const petObject of pets) {
 
-                if (petObject.id === parseInt(petId))        //petObject.city will be undifined pets have no city
-                    window.alert(`${petObject.name} barks at you`)
+            // What is the primary key of the clicked pet?
+            const[,petPrimaryKey] = itemClicked.id.split("--")
+
+                // Find the whole pet object to get the name 
+                let matchingPet = null
+                for (const pet of pets) {
+                    if (parseInt(petPrimaryKey) === pet.id) {
+                        matchingPet = pet
+                    }    
+                    
               }  
+              
+              // Find the related walker object assigned to the pet
+              let matchingWalker = null
+                for (const walker of walkers) {
+                    if (matchingPet.walkerId === walker.id) {
+                        matchingWalker = walker
+                    }
+                }
 
 
+              window.alert(`${matchingPet.name} is being walked by ${matchingWalker.name}`)
         }
     }
     
