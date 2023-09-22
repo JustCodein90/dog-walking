473d31587bba254a2e6668b7284419e51601b344
Justin Ireland <justinireland90@yahoo.com>
2023-09-21 22:23:37 -0400

    Updated Walkers.js to add extra cities to walker if

--- scripts/Walkers.js
+++ scripts/Walkers.js
@@ -1,58 +1,77 @@
-import { getWalkers } from "./database.js" // Missing a "s" should be getWalkers
+import { getWalkers, getWalkerCities, getCities } from "./database.js" // Missing a "s" should be getWalkers
 
 const walkers = getWalkers()
+const walkerCities = getWalkerCities()
+const cities = getCities()
 
 document.addEventListener(
     "click",  // This is the type of event
     (clickEvent) => {
-        /*
-            The target of a click event is the most specific HTML element
-            that was clicked by the user.
-        */
+        
         const itemClicked = clickEvent.target
 
-        /*
-            Only run the rest of the logic if a walker <li> was clicked
-        */
+        
         if (itemClicked.id.startsWith("walker")) {
 
-            /*
-                Extract the primary key from the id attribute of the list
-                item that you clicked on. Refer back to the code you
-                wrote for each list item. Note the format of the id
-                attribute ("walker--2" if you clicked on the second one).
-
-                This code splits that string apart into an array, and
-                captures the "2" and assigns it to be the value of the
-                `walkerId` variable.
-
-                Splitting a string in JavaScript:
-                    https://www.youtube.com/watch?v=u2ZocmM93yU
-
-                Destructuring in JavaScript:
-                    https://www.youtube.com/watch?v=UgEaJBz3bjY
-            */
+           
             const [,walkerId] = itemClicked.id.split("--")
 
-            /*
-                Now that you have the primary key of a walker object,
-                find the whole object by iterating the walkers array.
-            */
-            for (const walkerObject of walkers) {
+           
+           for (const walker of walkers) {
+                
+             if (walker.id === parseInt(walkerId)) {
+                const assignments = filterWalkerCitiesByWalker(walker)
+                const cities = assignedCityNames(assignments)
+             
 
-                /*
-                    Compare the primary key of each walker to the one
-                    you have. As soon as you find the right one, display
-                    the window alert message.
-                */
-                if (walkerObject.id === parseInt(walkerId)) {
-                    window.alert(`${walkerObject.name} services ${walkerObject.city}`)
-                }
+                window.alert(`${walker.name} services ${cities}`)
+             }
             }
         }
     }
 )
 
+
+
+// The function need the walker information, so define a parameter
+const filterWalkerCitiesByWalker = (walker) => {
+    // Define an empty array to store all of the assignment objects
+    const assignments = []
+
+    // Iterate the array value of walkerCities
+    for (const assignment of walkerCities) {
+
+        // Check if the primary key of the walker equals the foreign key on the assignment
+        if (assignment.walkerId === walker.id) {
+            // If it does, add the current object to the array of assignments
+            assignments.push(assignment)
+        }
+    }
+
+    // After the loop is done, return the assignments array
+    return assignments
+}
+
+// Define a function that builds a string of city names. Needs a paramter for assignments array.
+const assignedCityNames = (assignments) => {
+    // Define an empty string that will get appended with matching cities
+    let cityNames = ""
+
+    // Iterate the array of assignment objects
+    for (const assignment of assignments) {
+
+        // For each assignment, iterate the cities array to find the match
+        for (const city of cities) {
+            if (city.id === assignment.cityId) {
+                // Add the name of the matching city to the string of city names
+                cityNames = `${cityNames} and ${city.name}`
+            }
+        }
+    }
+
+    // After the loop is done, return the string
+    return cityNames
+}
 
 
 export const Walkers = () => {
@@ -67,4 +86,3 @@ export const Walkers = () => {
 
  return walkerHTML;
 }
-
